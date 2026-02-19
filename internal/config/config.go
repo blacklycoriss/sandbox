51e95623aad4b8169bdd2a1ad2632f16659b98c1
blacklycoriss <prokopovaleksandr544@gmail.com>
2026-02-19 21:40:36 +0700

    fix config.go: include only get envs

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,6 @@ package config
 
 import "os"
 
-func GetDBURL() string       { return os.Getenv("DB_URL") } // postgres://...
-func GetRedisAddr() string   { return "redis:6379" }
-func GetRabbitMQURL() string { return "amqp://guest:guest@rabbitmq:5672/" }
+func GetDBURL() string       { return os.Getenv("DB_URL") }
+func GetRedisAddr() string   { return os.Getenv("REDIS_ADDR") }
+func GetRabbitMQURL() string { return os.Getenv("RABBITMQ_URL") }
